5b3dd86c2c8331b66c45a84fde2ac787ed33ae8f
Maryana <renvema@bigmir.net>
2019-07-24 17:27:43 +0300

    commit2 HW28
    Have a nice day)

--- src/main/java/controller/UserRegistrationServlet.java
+++ src/main/java/controller/UserRegistrationServlet.java
@@ -17,36 +17,36 @@ import java.io.IOException;
 @WebServlet(value = "/add/user")
 public class UserRegistrationServlet extends HttpServlet {
 
-    private static final Logger logger = Logger.getLogger (UserRegistrationServlet.class);
-    private static final UserService userService = UserServiceFactory.getInstance ( );
+    private static final Logger logger = Logger.getLogger(UserRegistrationServlet.class);
+    private static final UserService userService = UserServiceFactory.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher ("/add_user.jsp").forward (req, resp);
+        req.getRequestDispatcher("/add_user.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String email = req.getParameter ("email");
-        String hashPassword = HashUtil.getSHA256SecurePassword (req.getParameter ("password"));
-        String hashRepeatPassword = HashUtil.getSHA256SecurePassword (req.getParameter ("repeatPassword"));
-        String role = req.getParameter ("role");
+        String email = req.getParameter("email");
+        String hashPassword = HashUtil.getSHA256SecurePassword(req.getParameter("password"));
+        String hashRepeatPassword = HashUtil.getSHA256SecurePassword(req.getParameter("repeatPassword"));
+        String role = req.getParameter("role");
 
-        if (email.isEmpty ( ) || email.isEmpty ( ) || hashPassword.isEmpty ( )) {
-            req.setAttribute ("error", "Empty fields!");
-            req.getRequestDispatcher ("/add_user.jsp").forward (req, resp);
-        } else if (hashPassword.equals (hashRepeatPassword)) {
-            User user = new User (IdGenerator.generateIdUser ( ), email, hashPassword, role);
-            userService.addUser (user);
-            resp.sendRedirect ("/admin/users");
+        if (email.isEmpty() || email.isEmpty() || hashPassword.isEmpty()) {
+            req.setAttribute("error", "Empty fields!");
+            req.getRequestDispatcher("/add_user.jsp").forward(req, resp);
+        } else if (hashPassword.equals(hashRepeatPassword)) {
+            User user = new User(IdGenerator.generateIdUser(), email, hashPassword, role);
+            userService.addUser(user);
+            resp.sendRedirect("/admin/users");
         } else {
-            req.setAttribute ("email", email);
-            req.setAttribute ("error", "You passwords not equals");
+            req.setAttribute("email", email);
+            req.setAttribute("error", "You passwords not equals");
 
-            req.getRequestDispatcher ("/add_user.jsp").forward (req, resp);
-            logger.info ("Passwords {" + hashPassword + "} and {" + hashRepeatPassword + "} not equals");
+            req.getRequestDispatcher("/add_user.jsp").forward(req, resp);
+            logger.info("Passwords {" + hashPassword + "} and {" + hashRepeatPassword + "} not equals");
         }
     }
 }
--- src/main/java/utils/HashUtil.java
+++ src/main/java/utils/HashUtil.java
@@ -6,29 +6,29 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class HashUtil {
+public class HashUtil{
 
-    private static final Logger logger = Logger.getLogger (HashUtil.class);
+    private static final Logger logger = Logger.getLogger(HashUtil.class);
 
-    public static String getSHA256SecurePassword(String passwordToHash) {
+    public static String getSHA256SecurePassword(String passwordToHash){
         MessageDigest digest = null;
-        try {
-            digest = MessageDigest.getInstance ("SHA-256");
-            byte[] encodedhash = digest.digest (passwordToHash.getBytes (StandardCharsets.UTF_8));
+        try{
+            digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
             return bytesToHex (encodedhash);
-        } catch (NoSuchAlgorithmException e) {
-            logger.error ("Can't hash password", e);
+        }catch (NoSuchAlgorithmException e){
+            logger.error("Can't hash password", e);
         }
         return null;
     }
 
-    private static String bytesToHex(byte[] hash) {
+    private static String bytesToHex(byte[] hash){
         StringBuffer hexString = new StringBuffer ( );
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString (0xff & hash[i]);
-            if (hex.length ( ) == 1) hexString.append ('0');
-            hexString.append (hex);
+        for (int i = 0; i < hash.length; i++){
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1)hexString.append('0');
+            hexString.append(hex);
         }
-        return hexString.toString ( );
+        return hexString.toString();
     }
 }
